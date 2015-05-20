1ea9bb65bf36c1f687c8d1916f5ccbdb1a6f8f68
aocampor <alocampor@gmail.com>
2015-05-20 23:22:13 +0200

    Adding MLP files with MVA info

---
+++ read_images/run_production.sh
@@ -0,0 +1,2 @@
+#./readImages_tagged /media/aocampor/MyDisk/DiabeticRetinophaty/train/ /home/aocampor/DiabeticRetinophaty/SalidaColorPixel15Per_MVAtest/ false
+./readImages_nontagged /media/aocampor/MyDisk/DiabeticRetinophaty/test/test/ /media/aocampor/MyDisk/DiabeticRetinophaty/SalidaNonTagged/ false
